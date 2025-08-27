69fe74be51edf6c0c9f21eedea0b9abb7e90bc7a
Brandon-Banh-858 <brandon.banh1@education.nsw.gov.au>
2025-08-28 09:14:14 +1000

    Com_mit

--- database_manager.py
+++ database_manager.py
@@ -3,6 +3,6 @@ import sqlite3 as sql
 def listExtension():
   con = sql.connect("database/data_source.db")
   cur = con.cursor()
-  data = cur.execute('SELECT * FROM extension').fetchall()
+  data = cur.execute('SELECT * FROM UserTable').fetchall()
   con.close()
   return data
