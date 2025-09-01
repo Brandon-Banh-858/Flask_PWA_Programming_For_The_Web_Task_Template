7cbcca1cabe3ec2756d44765bb4fecfbb38664d5
Brandon-Banh-858 <brandon.banh1@education.nsw.gov.au>
2025-09-02 09:41:27 +1000

    Commit_ed

--- database_manager.py
+++ database_manager.py
@@ -1,8 +1,8 @@
 import sqlite3 as sql
 
 def listExtension():
-  con = sql.connect("Flask_PWA_Programming_For_The_Web_Task_Template\Database\data_source.db")
-  cur = con.cursor()
-  data = cur.execute('SELECT * FROM UserTable').fetchall()
-  con.close()
-  return data
+    con = sql.connect("Flask_PWA_Programming_For_The_Web_Task_Template/Database/data_source.db")
+    cur = con.cursor()
+    data = cur.execute("SELECT * FROM UserTable").fetchall()
+    con.close()
+    return data
