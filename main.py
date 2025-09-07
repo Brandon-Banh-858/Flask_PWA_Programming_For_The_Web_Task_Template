2dc83c070096b4f61b4d19174c166d6ce9623705
Brandon-Banh-858 <brandon.banh1@education.nsw.gov.au>
2025-09-08 09:43:34 +1000

    Committ3

--- main.py
+++ main.py
@@ -6,11 +6,25 @@ import database_manager as dbHandler
 app = Flask(__name__)
 
 @app.route('/index.html', methods=['GET'])
-@app.route('/add.html', methods=['GET'])
 @app.route('/', methods=['POST', 'GET'])
+
 def index():
    data = dbHandler.listExtension()
    return render_template('/index.html', content=data)
+
+@app.route('/add.html', methods=['GET'])
+@app.route('/', methods=['POST', 'GET'])
+def add():
+   data = dbHandler.listExtension()
+   return render_template('/partials/add.html', content=data)
+
+@app.route('/homepage.html', methods=['GET'])
+@app.route('/', methods=['POST', 'GET'])
+
+def homepage():
+   data = dbHandler.listExtension()
+   return render_template('/partials/homepage.html', content=data)
+
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=5000)
