523e2c52e99e9efd3eb1360ce9b4d6508c2d760f
Brandon-Banh-858 <brandon.banh1@education.nsw.gov.au>
2025-09-15 12:57:09 +1000

    Commitscript

---
+++ function.js
@@ -0,0 +1,18 @@
+const PyButton = document.querySelector("div.div.button[id*='Python']")
+const JavaButton = document.querySelector("div.div.button[id*='Java']")
+const JScriptButton = document.querySelector("div.div.button[id*='Javascript']")
+const CsButton = document.querySelector("div.div.button[id*='C#']")
+const CppButton = document.querySelector("div.div.button[id*='C++']")
+const SqlButton = document.querySelector("div.div.button[id*='SQL']")
+const HtmlButton = document.querySelector("div.div.button[id*='Html']")
+const CssButton = document.querySelector("div.div.button[id*='Css']")
+
+const Button = document.querySelector("div.div.button")
+
+function createOverlay(buttonClick) {
+    print("a")
+    buttonClick.target.h1.id = "a"
+}
+PyButton.addEventListener("click", buttonClick)
+
+print("b")
